714ad0a0f4323209e51ac7cece5b2d11877091c7
João Vitor <jvitorlouzada@gmail.com>
2023-09-26 11:52:29 -0300

    Ajustes na pontuação e ver tabela
    Adicionando a logica na parte da pontuação e na parte de ver tabela.
    Corrigindo algumas outras partes.

--- Campeonato_Brasileiro.cs
+++ Campeonato_Brasileiro.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 
-class Campeonato_Brasileiro
+class Time
 {
-        static List<string> ListaTimes = new List<string>();
+    public string Nome { get; set; }
+    public int Pontuacao { get; set; }
+    public int QuantidadeJogos { get; set; }
+
+    public Time(string nome)
+    {
+        Nome = nome;
+        Pontuacao = 0;
+        QuantidadeJogos = 0;
+    }
+}
+
+
+class Campeonato_Brasileiro
+{        
+        static List<Time> ListaTimes = new List<Time>();
         static void Main(string[] args)
     {
         Console.WriteLine("Campeonato Brasileiro\n");
         MenuPrincipal();
     }
-
-
     static void MenuPrincipal(){
 
         Console.WriteLine("Digite 1 para adicionar um time");
@@ -32,7 +45,7 @@ class Campeonato_Brasileiro
             case 3: AdicionarPontuacao();
                 break;
 
-        case 0:
+            case 0:
                 Environment.Exit(0);
                 break;
 
@@ -41,41 +54,78 @@ class Campeonato_Brasileiro
                 break;
         }  
     }   
-
     static void AdicionarTime() {
         Console.Clear();
         Console.Write("Digite o nome do time para adicionar: ");
-        string Time = Console.ReadLine();
+        string nomeTime = Console.ReadLine()!;
 
-        ListaTimes.Add(Time); // Adicionado o time na lista de times.
+        Time novoTime = new Time(nomeTime);
+        ListaTimes.Add(novoTime);
 
-        Console.WriteLine($"Time '{Time}' foi adicionado com sucesso!\n");
+        Console.WriteLine($"Time '{nomeTime}' foi adicionado com sucesso!\n");
         MenuPrincipal(); // Após adicionar o time, voltamos ao menu principal.
-    
-
     }
-
-    static void VerTabela() {
+    static void VerTabela()
+    {
         Console.Clear();
         Console.WriteLine("Tabela do Campeonato:\n");
 
-        foreach (string Time in ListaTimes){
-            Console.WriteLine($"Time: {Time}");
+        foreach (var time in ListaTimes)
+        {
+            Console.WriteLine($"Time: {time.Nome}, Pontuação: {time.Pontuacao}, Jogos: {time.QuantidadeJogos}");
         }
-      
-        Console.WriteLine("A tabela está vazia.\n");
-        MenuPrincipal(); // Após ver a tabela, voltamos ao menu principal.
-    
+
+        if (ListaTimes.Count == 0)
+            Console.WriteLine("A tabela está vazia.\n");
+
+        MenuPrincipal();
     }
 
-    static void AdicionarPontuacao() {
-        Console.Clear();
-        Console.WriteLine("Adicionar Pontuação da Rodada:\n");
+    static void AdicionarPontuacao()
+{
+    Console.Clear();
+    Console.Write("Qual time?\n");
+    string nomeTime = Console.ReadLine()!;
 
-        // Aqui preciso adicionar a lógica para adicionar a pontuação de uma rodada.
-        Console.WriteLine("Pontuação da rodada foi adicionada com sucesso!\n");
-        MenuPrincipal(); // Após adicionar a pontuação, voltamos ao menu principal.
-    
+    Time timeSelecionado = ListaTimes.Find(time => time.Nome == nomeTime);
 
+    if (timeSelecionado != null)
+    {
+        Console.Write($"O {nomeTime} Ganhou? Perdeu? Empatou? :\n");
+        string pontuacao = Console.ReadLine()!;
+
+        switch (pontuacao.ToLower())
+        {
+            case "ganhou":
+                timeSelecionado.Pontuacao += 3;
+                Console.WriteLine("Pontuação da rodada foi adicionada com sucesso!\n");
+                break;
+
+            case "empatou":
+                timeSelecionado.Pontuacao += 1;
+                Console.WriteLine("Pontuação da rodada foi adicionada com sucesso!\n");
+                break;
+
+            case "perdeu":
+                Console.WriteLine("O time perdeu, não houve pontuação!\n");
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida. Tente novamente.");
+                break;
+        }
+
+        timeSelecionado.QuantidadeJogos++;
     }
+    else
+    {
+        Console.WriteLine($"Time '{nomeTime}' não encontrado.\n");
+    }
+
+    MenuPrincipal();
 }
+
+
+
+
+    }
